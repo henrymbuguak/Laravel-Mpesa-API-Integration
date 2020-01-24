f34e7a568a10fa4016160cbcb0a4b6f358a5826a
Henry Mbugua <hmbugua@resolution.co.ke>
2020-01-24 17:24:46 +0300

    Stk push complete

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -2,10 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class MpesaController extends Controller
 {
+
+    /*
+     * Lipa na M-PESA password
+     * */
+
+    public function lipaNaMpesaPassword()
+    {
+        $lipa_time = Carbon::rawParse('now')->format('YmdHms');
+        $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
+        $BusinessShortCode = 174379;
+        $timestamp =$lipa_time;
+
+        $lipa_na_mpesa_password = base64_encode($BusinessShortCode.$passkey.$timestamp);
+        return $lipa_na_mpesa_password;
+    }
+
+
+    /*
+     * Lipa na M-PESA STK Push method
+     * */
+
+    public function customerMpesaSTKPush()
+    {
+        $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer '.$this->generateAccessToken()));
+
+
+        $curl_post_data = [
+            //Fill in the request parameters with valid values
+            'BusinessShortCode' => 174379,
+            'Password' => $this->lipaNaMpesaPassword(),
+            'Timestamp' => Carbon::rawParse('now')->format('YmdHms'),
+            'TransactionType' => 'CustomerPayBillOnline',
+            'Amount' => 5,
+            'PartyA' => 254728851119, // replace this with your phone number
+            'PartyB' => 174379,
+            'PhoneNumber' => 254728851119, // replace this with your phone number
+            'CallBackURL' => 'https://blog.hlab.tech/',
+            'AccountReference' => "H-lab tutorial",
+            'TransactionDesc' => "Testing stk push on sandbox"
+        ];
+
+        $data_string = json_encode($curl_post_data);
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+
+        $curl_response = curl_exec($curl);
+
+        return $curl_response;
+    }
+
+
     public function generateAccessToken()
     {
         $consumer_key="sMpgnYW62glBlxPXbyTBEGdPib8eJLOL";
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('v1/access/token', 'MpesaController@generateAccessToken');
+Route::post('v1/hlab/stk/push', 'MpesaController@customerMpesaSTKPush');
